3acff1edd5fde330a52746888508f06eede34625
swapnakochrekar35 <155220686+swapnakochrekar35@users.noreply.github.com>
2023-12-30 13:52:51 +0000

    js variable file added.

---
+++ 01_jsbasic/01_variable.js
@@ -0,0 +1,7 @@
+const accountId=1233434;//we can,t change the value of const
+let accountEmail="swap@gmail.com";
+var accountPassword="12345677";//don't use var due to scope problem
+accountCity="udaypur";
+let accountState;
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
