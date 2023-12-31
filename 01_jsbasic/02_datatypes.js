353a7511e3144790eac4a65574f41a5628061ae5
swapnakochrekar35 <155220686+swapnakochrekar35@users.noreply.github.com>
2023-12-31 06:56:49 +0000

    datatypes

---
+++ 01_jsbasic/02_datatypes.js
@@ -0,0 +1,11 @@
+let name="swapna kochrekar";//string
+
+let age=21;//number
+
+let loggedIn=true;//boolean
+
+let  email=undefined;//undefined
+ 
+let password=null;//null
+
+console.table([typeof name,typeof age,typeof loggedIn,typeof email, typeof password])
