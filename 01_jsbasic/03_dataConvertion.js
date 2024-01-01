3eb0900b18f8e21dc15853aa5769a2671bfd855b
swapnakochrekar35 <155220686+swapnakochrekar35@users.noreply.github.com>
2024-01-01 13:20:42 +0000

    data conversion

---
+++ 01_jsbasic/03_dataConvertion.js
@@ -0,0 +1,15 @@
+let score=45;
+console.log(typeof score)
+
+//convert score in to string
+
+let valueInstring=String(score)
+console.log( typeof valueInstring)
+
+let isLogedIn=true;
+console.log(isLogedIn);
+
+//convert boolean in to string
+
+let booleanInString=String(isLogedIn);
+console.log(typeof booleanInString)
